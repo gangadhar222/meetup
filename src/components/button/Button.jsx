ccdc7dd5abc2474ca752078443a1d45a603b4bde
gangadhar222 <gangadhar@helloar.io>
2020-10-23 23:30:12 +0530

    documentation

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,5 +3,5 @@ import styles from "./Button.module.css";
 
 export default function Button(props) {
   const { label } = props;
-  return <button className={styles.btn}>{label}</button>;
+  return <button className={styles.btn} data-testid="button" >{label}</button>;
 }
